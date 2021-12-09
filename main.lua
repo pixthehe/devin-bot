1df8d5f8a7dba31d22199e733df9011c60713811
pixlovesit@gmail.com <pixlovesit@gmail.com>
2021-12-08 19:39:41 -0500

    'add'

---
+++ main.lua
@@ -0,0 +1,28 @@
+--* Init
+local discordia = require("discordia")
+local client = discordia.Client()
+
+--* Varibles
+local prefix = "-"
+
+--[[
+TODO: Add furry % cmd to bot
+]] 
+
+--* Main if found message fundtion
+client:on("messageCreate", function(message)
+    local content = message.content
+    local member = message.member
+    local memberId = message.member.id
+
+    if content:lower() == prefix.."ping" then
+        message:reply("pong")
+    end
+    if content:lower() == prefix.."furry" then
+        message:reply("You are "..math.random(1,100).."% a furry")
+    end
+end)
+
+
+
+client:run("Bot "..io.open("./token"):read())
